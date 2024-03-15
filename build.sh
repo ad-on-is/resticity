b4bc34a25eb5b32fec92d2322b256c0703f7ebb2
Adis Durakovic <adis.durakovic@gmail.com>
2024-03-15 12:21:33 +0100

    chore: workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-VERSION=$(git describe --tags)
+VERSION=$2
+
+if [ -z "$VERSION" ]; then
+  VERSION=$(git describe --tags)
+fi
+
+echo $VERSION
+exit
 BUILD=$(date +%FT%T%z)
 LD_FLAGS="-X main.Version=$VERSION -X main.Build=$BUILD"
+wails=$(which wails)
 
 case $1 in
   "desktop")
-    CGO_ENABLED=0 wails build -ldflags="$LD_FLAGS"
+    CGO_ENABLED=0 $wails build -ldflags="$LD_FLAGS"
     ;;
   "server")
     CGO_ENABLED=0 go build -ldflags="$LD_FLAGS" ./cmd/server
@@ -15,7 +23,7 @@ case $1 in
     cd frontend && pnpm install && pnpm build
     ;;
   "dev")
-    wails dev -ldflags="$LD_FLAGS" -loglevel "Error"
+    $wails dev -ldflags="$LD_FLAGS" -loglevel "Error"
     ;;
   *)
     ;;
